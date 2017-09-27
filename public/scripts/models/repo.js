eba0cdbe8ecab39a2e6d10875da88a07aab91a4a
Kinley Ramson <kinleyramson@gmail.com>
2017-09-27 16:26:41 -0700

    added constring, attempting the ajaz todo

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -11,10 +11,9 @@ var app = app || {};
     //       Don't forget to remove the headers from our request - we're no longer using a token on the
     //       client side of our app, our new proxyGitHub function will be handling the token using our
     //       new environment variable!
-    $.ajax({
-      url: `https://api.github.com/user/repos`,
-      type: 'GET',
-      headers: {'Authorization': `token ${githubToken}`}
+    $.get({
+      url: `/github/user/repos`,
+      type: 'GET'
     })
     .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
     .then(callback);
